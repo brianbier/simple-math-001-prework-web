8985ed80e4665544c6d1817134151103a75efda0
brianbier <briansbier@gmail.com>
2015-09-07 17:43:15 -0400

    simple math problems

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,29 @@
 def addition(num1, num2)
+  num1 + num2
 end
-
+addition(5,5)
 def subtraction(num1, num2)
+  num1 - num2
 end
 
 def division(num1, num2)
+  num1 / num2
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 end
 
 def modulo(num1, num2)
+  num1 % num2
 end
 
 def square_root(num)
+  Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+  num1+((num2 * num3) / num4)
 end
+order_of_operation(2,4,4,1)
